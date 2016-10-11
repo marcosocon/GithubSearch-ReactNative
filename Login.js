d324adb5e70928bedba368ff1aa1794d7c43e216
marcosocon <oconmarcos97@gmail.com>
2016-10-10 23:15:27 -0300

    added styles and form entries

--- Login.js
+++ Login.js
@@ -5,15 +5,23 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
   Image,
   View
 } from 'react-native';
 
 const styles = StyleSheet.create({
-  stretch: {
-    width: 200,
-    height: 200
-  }
+	stretch: {
+		width: 200,
+		height: 200
+	},
+	heading:{
+		fontSize:30,
+	},
+	input:{
+		height:50
+	}
+
 });
 
 class Login extends Component {
@@ -21,7 +29,9 @@ class Login extends Component {
     return (
 		<View style={{alignItems: 'center', padding:50}}>
 			<Image style={styles.stretch} source={require('./img/octocat.png')} />
-			<Text>Github Search</Text>
+			<Text style={styles.heading}>Github Search</Text>
+			<TextInput style={styles.input} placeholder="GitHub Username"/>
+			<TextInput style={styles.input} secureTextEntry="true" placeholder="GitHub Password"/>
 		</View>
 
     );
